e43f9f9a1000edb355ebf427ff1abdd808d490d1
sumitnlab <65151890+sumitnlab@users.noreply.github.com>
2020-12-06 15:06:08 +0530

    Update Program.cs

--- Assignment4/Que3/Program.cs
+++ Assignment4/Que3/Program.cs
@@ -6,32 +6,132 @@ using System.Threading.Tasks;
 
 namespace Que3
 {
+   
+      public struct Student
+    {
+
+        private string name;
+
+          public string StudName
+        {
+            set
+            {
+                 if(value!=null)
+                {
+                    name = value;
+                    
+                }
+                 else
+                {
+                     Console.WriteLine("Invalid name");
+                }
+
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+
+        private int rollno;
+
+        public int studrollno
+        {
+            set
+            {
+                if(value>=0)
+                {
+                    rollno = value;
+                }
+            }
+
+            get
+            {
+                return rollno;
+            }
+        }
+
+        private decimal marks;
+
+            public decimal studMarks
+        {
+            set
+            {
+                if(value!=0)
+                {
+                    marks = value;
+                }
+                else
+                {
+                    Console.WriteLine("Marks are Invalid");
+                }
+            }
+            get
+            {
+                return marks;
+            }
+        }
+
+        public Student(string name1 , int rollno1 , decimal marks1) : this()
+        {
+           
+            this.StudName = name1;
+            this.studrollno = rollno1;
+            this.studMarks = marks1;
+
+
+        }
+
+
+        public void show()
+        {
+            
+            Console.WriteLine(StudName);
+            Console.WriteLine(studrollno);
+            Console.WriteLine(studMarks);
+        }
+
+    }
     class Program
     {
-        static void Main4()
+        static void Main(string[] args)
         {
+            Console.WriteLine("How many student you want : ");
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            Student[] s1 = new Student[capacity];
+
+            for(int i = 0; i<s1.Length;i++)
+            {
+                Console.WriteLine("\nEnter Student name : ");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Student rollno : ");
+
+                int rollno = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEnter Student Marks : ");
+
+                decimal marks = decimal.Parse(Console.ReadLine());
+
+                Student temp = new Student(name, rollno, marks);
+
+                s1[i] = temp;
+            }
+
+            Console.WriteLine("\n*****Details of Student*****\n");
+             
+              for(int i = 0; i<s1.Length;i++)
+            {
+                s1[i].show();
+            }
 
             Console.ReadLine();
         }
 
-    }
-    public struct Student
-    {
-        private string name;
-        private int rollNo;
-        private decimal marks;
-
-        // public string Name { get { return name; } set { if (value == null) Console.WriteLine("Invalid Name "); else name = value; } }
-        public string Name { get; set; }
-        // public int RollNo { get { return rollNo; } set { if (value > 0) rollNo = value; else Console.WriteLine("Invalid RollNo"); } }
-        public int RollNo { get; set; }
-        public decimal Marks { get; set; }
-
-        public Student(string name, int rollNo, decimal marks)
-        {
-            this.Name = name;
-            this.RollNo = rollNo;
-            this.Marks = marks;
-        }
+         
     }
 }
